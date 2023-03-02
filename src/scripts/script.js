2fe2e11ca0d593fa86276c84358eb858f58b0491
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-02 11:30:58 -0300

    mobile JS

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,25 @@
 mobileMenuIcon.addEventListener("click", () => {
-    const check = menu.classList.contains("hidden")
+  const check = menu.classList.contains("hidden");
 
-    menu.classList.toggle("hidden",!check)
-    menu.classList.toggle("block",check)
-    overlay.classList.toggle("hidden",!check)
-    overlay.classList.toggle("fixed",check)
+  menu.classList.toggle("hidden", !check);
+  menu.classList.toggle("block", check);
+  overlay.classList.toggle("hidden", !check);
+  overlay.classList.toggle("fixed", check);
 
-    mobileMenuIcon.src = !check
-      ? "../images/icon-hamburger.svg"
-      : "../images/icon-close-menu.svg";
+  mobileMenuIcon.src = !check
+    ? "../images/icon-hamburger.svg"
+    : "../images/icon-close-menu.svg";
+});
+
+let bookmark = false;
+
+bookmarkIcon.addEventListener("click", () => {
+  bookmarkIcon.src = bookmark
+    ? "../images/icon-bookmark.svg"
+    : "../images/icon-bookmarked.svg";
+  bookmark = !bookmark;
+});
+
+backProject.addEventListener("click", () => {
+    
 })
